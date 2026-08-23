3ca49940effa15b981ae5a79b273c426a74851e1
Yash Creations <yashransubhe@gmail.com>
2026-08-23 12:08:39 +0530

    make landing page the public entry point

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,12 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import WorldClassApp from "./WorldClassApp";
+import RootExperience from "./RootExperience";
 import "./styles.css";
 import "./workbench.css";
 import "./landing.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <WorldClassApp />
+    <RootExperience />
   </React.StrictMode>,
 );

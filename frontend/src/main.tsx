a1a92c934a16d41cad445588a4b4c3b847341727
Yash Creations <yashransubhe@gmail.com>
2026-08-23 12:08:16 +0530

    load landing page styles

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import WorldClassApp from "./WorldClassApp";
 import "./styles.css";
 import "./workbench.css";
+import "./landing.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
